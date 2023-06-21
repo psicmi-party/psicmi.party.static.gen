d8daf9d6c12cd4a7f3375212f0ab83ec2cea232b
psicmi <psicmi@163.com>
2023-06-21 17:14:24 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -247,6 +247,8 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     extractHeaders: ['h2', 'h3', 'h4', 'h5', 'h6'], // 提取标题到侧边栏的级别，默认['h2', 'h3']
   },
 
+  permalink: '/:year/:month/:day/:slug',
+
   // 监听文件变化并重新构建
   extraWatchFiles: [
     '.vuepress/config.ts',
